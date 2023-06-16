9e3aa8d9db1f0258cb2c02a02484850ac56952d3
Nguyen Quang Hao <haolk4@gmail.com>
2023-06-16 18:13:20 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world React.js (Hao)
         </p>
         <a
           className="App-link"
