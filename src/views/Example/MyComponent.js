6d3811cce5411dc2ecd6ba60345660b559d57528
Nguyen Quang Hao <haolk4@gmail.com>
2023-06-19 16:07:32 +0900

    học state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: "Messi",
+        channel: "học reactjs"
+    }
+
+    /*
+     JSX => return block
+
+     */
+
+     handleOnChangeName = (event) => {
+        this.setState({ name: event.target.value });
+     }
+
     render() {
         return (
-            <div>Hello my Component Hao Nguyen</div>
+            <>
+                <div className='first'>
+                   <input value={this.state.name} type='text' onChange={(event) => this.handleOnChangeName(event)} />
+                    My name is {this.state['name']}
+                </div>
+                <div className='second'>
+                   Chào mừng đến với channel: {this.state.channel}
+                </div>
+            </>
         )
     }
 }
