0a4ccedadbfc78b3f659e6e47d72a39d53064d4c
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-12 15:08:10 +0900

    chuyển trang(redirect) với reactjs router

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { withRouter } from "react-router";
+
 class Home extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.history.push("/todo");
+    }, 3000);
+  }
+  //HOC: higher order component
   render() {
+    console.log(this.props);
     return <div>hello world from homepage with Hao</div>;
   }
 }
-export default Home;
+
+export default withRouter(Home);
