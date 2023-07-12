c08c3cb07badeb4104a36b52f5e96f350ca29b3d
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-12 14:01:20 +0900

    điều hướng trang với react router

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.scss";
-// import MyComponent from "./Example/MyComponent.js";
+import MyComponent from "./Example/MyComponent.js";
 import ListTodo from "./Todos/ListTodo";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Nav from "./Nav/Nav";
+import Home from "./Example/Home";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 /**
  * 2 components: class component / function component (function, arrow)
  * JSX
@@ -13,26 +16,37 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   //const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Simple TODO app with React.js</p>
-        {/* <MyComponent /> */}
-        <ListTodo />
-      </header>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/todo">
+              <ListTodo />
+            </Route>
+            <Route path="/about">
+              <MyComponent />
+            </Route>
+          </Switch>
+        </header>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Example/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return <div>hello world from homepage with Hao</div>;
+  }
+}
+export default Home;
