14f84a6900523ddcacdbc90dc29fb3dc2ded85f5
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-12 18:00:33 +0900

    router parameter sử dụng tham số trên url

--- src/views/App.js
+++ src/views/App.js
@@ -5,6 +5,7 @@ import ListTodo from "./Todos/ListTodo";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ListUser from "./Users/ListUser";
+import DetailUser from "./Users/DetailUser";
 
 import Nav from "./Nav/Nav";
 import Home from "./Example/Home";
@@ -32,8 +33,11 @@ function App() {
             <Route path="/about">
               <MyComponent />
             </Route>
-            <Route path="/user">
+            <Route path="/user" exact>
               <ListUser />
+            </Route>
+            <Route path="/user/:id">
+              <DetailUser />
             </Route>
           </Switch>
         </header>
