ebb6f52fc7148443091c4f1dc714f6a1c3bff89e
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-13 10:21:44 +0900

    Setup redux vào dự án

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+  users: [
+    { id: 1, name: "hao" },
+    { id: 2, name: "chau" },
+  ],
+};
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
