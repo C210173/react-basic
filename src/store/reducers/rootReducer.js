1187c79559886dcae381444126a221e70fe97dfc
Nguyen Quang Hao <haolk4@gmail.com>
2023-07-13 10:41:24 +0900

    Mapping State(redux) to Props(react)

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,6 +2,7 @@ const initState = {
   users: [
     { id: 1, name: "hao" },
     { id: 2, name: "chau" },
+    { id: 2, name: "hehe" },
   ],
 };
 const rootReducer = (state = initState, action) => {
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
 import image from "../../assets/images/fort.jpg";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
   componentDidMount() {
@@ -11,7 +12,7 @@ class Home extends React.Component {
   }
   //HOC: higher order component
   render() {
-    console.log(this.props);
+    console.log("check props", this.props.dataRedux);
     return (
       <>
         <div>hello world from homepage with Hao</div>
@@ -23,5 +24,11 @@ class Home extends React.Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    dataRedux: state.users,
+  };
+};
+
 // export default withRouter(Home);
-export default Color(Home);
+export default connect(mapStateToProps)(Color(Home));
